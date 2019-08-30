1edf554837a13f9ceb3eca0c849caf367d26d0dd
brunocsouzasantos <brunocsouzasantos@gmail.com>
2019-08-29 21:18:39 -0300

    Aula 14

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-
-               <painel titulo="Dashboard">
+        <pagina tamanho="10">
+                <painel titulo="Dashboard">
 
                 Teste de conteúdo...
-                    <div class="row">
+                        <div class="row">
                         <div class="col-md-4">
-                        <painel titulo="Conteúdo 1" cor="blue">
+                                <caixa qtd="80" titulo="Artigos" url="{{route('artigos.index')}}" cor="orange" icone="ion ion-pie-graph">
 
-                                Teste de conteúdo...
-                
-                        </painel>
-                    </div>
-                    <div class="col-md-4">
-                            <painel titulo="Conteúdo 2" cor="orange">
-    
-                                    Teste de conteúdo...
-                    
-                            </painel>
-                    </div>
-                    <div class="col-md-4">
-                            <painel titulo="Conteúdo 3" cor="panel-warning">
-    
-                                    Teste de conteúdo...
-                    
-                            </painel>
-                    </div>
+                                </caixa>
+                        </div>
+                        <div class="col-md-4">
+                                <caixa qtd="150" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker">
+
+                                </caixa>
+                        </div>
+                        <div class="col-md-4">
+                                <caixa qtd="3" titulo="Autores" url="#" cor="red" icone="ion ion-person">
+
+                                </caixa>
+                        </div>
                 </div>
-               </painel>
-            </div>
-        </div>
-    </div>
-</div>
+                </painel>
+        </pagina>
 @endsection
